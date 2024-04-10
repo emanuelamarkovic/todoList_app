bff5a65665c2728b6004a983a5adae4f5d8021e5
emanuela <emanuela.markovic@hotmail.com>
2024-04-10 09:29:30 +0200

    Implementation of a React component with ScrollView for displaying tasks, pressable elements for selecting task categories and an image in the event that no tasks are available.

--- todoList/app/(tabs)/home/index.js
+++ todoList/app/(tabs)/home/index.js
@@ -1,11 +1,106 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  ScrollView,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+} from "react-native";
 import React from "react";
+import { AntDesign } from "@expo/vector-icons";
 
 const index = () => {
+  const todos = [];
   return (
-    <View>
-      <Text>index</Text>
-    </View>
+    <>
+      <View
+        style={{
+          marginHorizontal: 10,
+          marginVertical: 10,
+          flexDirection: "row",
+          alignItems: "center",
+          gap: 12,
+        }}
+      >
+        <Pressable
+          style={{
+            backgroundColor: "#7CB9EB",
+            paddingHorizontal: 10,
+            paddingVertical: 6,
+            borderRadius: 25,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Text style={{ color: "white", textAlign: "center" }}>All</Text>
+        </Pressable>
+        <Pressable
+          style={{
+            backgroundColor: "#7CB9EB",
+            paddingHorizontal: 10,
+            paddingVertical: 6,
+            borderRadius: 25,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Text style={{ color: "white", textAlign: "center" }}>Work</Text>
+        </Pressable>
+        <Pressable
+          style={{
+            backgroundColor: "#7CB9EB",
+            paddingHorizontal: 10,
+            paddingVertical: 6,
+            borderRadius: 25,
+            alignItems: "center",
+            justifyContent: "center",
+            marginRight: "auto",
+          }}
+        >
+          <Text style={{ color: "white", textAlign: "center" }}>Family</Text>
+        </Pressable>
+        <Pressable>
+          <AntDesign name="pluscircle" size={30} color="#007FFF" />
+        </Pressable>
+      </View>
+      <ScrollView style={{ flex: 1, backgroundColor: "#e0f2fe" }}>
+        <View style={{ padding: 10 }}>
+          {todos?.length > 0 ? (
+            <View></View>
+          ) : (
+            <View
+              style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+                marginTop: 50,
+                marginLeft: "auto",
+                marginRight: "auto",
+              }}
+            >
+              <Image
+                style={{ width: 300, height: 300, resizeMode: "contain" }}
+                source={require("../../../assets/note-5913650_1280.png")}
+              />
+              <Text
+                style={{
+                  textAlign: "center",
+                  fontSize: 16,
+                  color: "black",
+                  marginTop: 20,
+                }}
+              >
+                {" "}
+                No tasks for Today! add a task
+              </Text>
+              <Pressable style={{ marginTop: 15 }}>
+                <AntDesign name="pluscircle" size={30} color="#007FFF" />
+              </Pressable>
+            </View>
+          )}
+        </View>
+      </ScrollView>
+    </>
   );
 };
 
