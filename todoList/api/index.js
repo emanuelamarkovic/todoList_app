7fb6e4b58868a14cbaec939954db0a69dc8c2023
emanuela <emanuela.markovic@hotmail.com>
2024-04-10 14:22:39 +0200

    Adding the registration routes with the controller logic and the database connection.

--- todoList/api/index.js
+++ todoList/api/index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import bodyParser from "body-parser";
-import "./db-connection.js";
 import crypto from "crypto";
 import "./config.js";
 import cors from "cors";
+import handleRegistration from "./controller/usersControllers.js";
+import "./db-connection.js";
 
 const app = express();
 const port = process.env.PORT;
@@ -12,6 +13,8 @@ app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.post("/register", handleRegistration);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port} \n`);
